573ad9f094abe2ec08348b31cbbe26f1949f862d
Andreas Frischauf <andreas.frischauf@fullstacks.eu>
2026-02-26 08:23:40 +0100

    feat: initial push

---
+++ SA-Exoscale_fs/README.md
@@ -0,0 +1,1 @@
+# SA-Exoscale_fs
---
+++ SA-Exoscale_fs/additional_packaging.py
@@ -0,0 +1,26 @@
+from os.path import sep, exists, dirname, realpath, join
+from os import remove, system, _exit, WEXITSTATUS
+
+
+def additional_packaging(ta_name=None):
+    pass
+
+
+def cleanup_output_files(output_path: str, ta_name: str) -> None:
+    """
+    prepare a list for the files to be deleted after the source code has been copied to output directory
+    :param output_path: The path provided in `--output` argument in ucc-gen command or the default output path.
+    :param ta_name: The add-on name which is passed as a part of `--addon-name` argument during `ucc-gen init`
+                    or present in app.manifest file of add-on.
+    """
+    files_to_delete = []
+    files_to_delete.append(
+        sep.join([output_path, ta_name, "bin", "audit_helper_test.py"])
+    )
+
+    for delete_file in files_to_delete:
+        try:
+            remove(delete_file)
+        except FileNotFoundError:
+            # simply pass if the file doesn't exist
+            pass
